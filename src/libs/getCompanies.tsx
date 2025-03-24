1e6cb699abbbd0accea0a49522220d72bea757fb
Karin1844 <Karinsitthibut@gmail.com>
2025-03-25 03:28:52 +0700

    bug fix

--- src/libs/getCompanies.tsx
+++ src/libs/getCompanies.tsx
@@ -1,13 +1,11 @@
 export default async function getCompanies() {
-    await new Promise((resolve) => setTimeout(resolve, 300));
 
-    const response = await fetch("http://localhost:5000/api/v1/companies", {
-        cache: "no-store", 
-    });
+    await new Promise( (resolve) => setTimeout(resolve, 300) )
 
-    if (!response.ok) {
-        throw new Error("Failed to fetch companies");
+    const response = await fetch("http://localhost:5000/api/v1/companies")
+    if(!response.ok) {
+        throw new Error("Failed to fetch cars")
     }
 
-    return await response.json();
-}
+    return await response.json()
+ }
