0f3788145b71a64abf3971c3272e91f8d199773f
KhanesT <khanes96.bb@gmail.com>
2025-03-24 11:51:32 +0700

    Remove BannerButton (use in Banner)

--- src/components/HomePage/Banner.tsx
+++ src/components/HomePage/Banner.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
-import BannerButton from './BannerButton';
+import Link from 'next/link'
+
 
 export default function Banner () {
     return (
         <div>
             <div className='block p-1 m-0 w-screen h-[80vh] relative'>
+                
                 <Image 
                     src="/img/banner.jpg"
                     alt='banner'
@@ -20,8 +22,16 @@ export default function Banner () {
                     <div className='text-2xl font-medium mt-10 w-[40%]'> 
                         Build your future with a job you love. Explore career opportunities here. 
                     </div>
-                    <BannerButton/>
+                    <div className='mt-10'>
+                        <Link className="rounded-lg bg-primary py-2 px-5 text-white
+                            hover:shadow-xl hover:shadow-secondary transition duration-200"
+                            href='/company' 
+                        >
+                            Explore
+                        </Link>
+                    </div>
                 </div>
+
             </div>
         </div>
     )
