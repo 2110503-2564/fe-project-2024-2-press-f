cd60378b48a4b54256e0eae87840fa47831c9b2b
Purinut Jamma <poorinut.frame2547@gmail.com>
2025-03-25 20:08:41 +0700

    If not signin redirect to signin

--- src/app/(mybookinginfo)/mybooking/page.tsx
+++ src/app/(mybookinginfo)/mybooking/page.tsx
@@ -2,11 +2,14 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions"
 import { getServerSession } from "next-auth"
 import getInterviews from "@/libs/manageInterview/getInterviews"
 import InterviewList from "@/components/Interview/InterviewList"
+import { redirect } from "next/navigation"
 
 export default async function MyBookingPage() {
 
     const session = await getServerSession(authOptions)
-    if (!session || !session.user.token) return null
+    if (!session || !session.user.token) {
+        redirect('/api/auth/signin?callbackUrl=/mybooking')
+    }
     
     const interview = await getInterviews(session.user.token)
 
