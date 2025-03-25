4d41627d9d462fbc945e653d962c2d3b25aeca1f
Purinut Jamma <poorinut.frame2547@gmail.com>
2025-03-25 20:29:23 +0700

    Update [wid] page

--- src/app/(mybookinginfo)/mybooking/[wid]/page.tsx
+++ src/app/(mybookinginfo)/mybooking/[wid]/page.tsx
@@ -3,6 +3,8 @@ import getInterview from "@/libs/manageInterview/getInterview";
 import dayjs from "dayjs";
 import { getServerSession } from "next-auth";
 import { InterviewItem } from "../../../../../interface";
+import UpdateComponent from "@/components/Interview/UpdateComponent";
+import DeleteComponent from "@/components/Interview/DeleteComponent";
 
 export default async function InterviewDetailPage({ params }: { params: { wid: string } }) {
     
@@ -20,11 +22,18 @@ export default async function InterviewDetailPage({ params }: { params: { wid: s
     function formatDate(dateString: string): string {
             return dayjs(dateString).format('YYYY/MM/DD HH:mm');
         }
-
     return (
         <main className="p-10 bg-gray-100 min-h-screen">
             <div className="max-w-4xl mx-auto bg-white p-6 rounded-lg shadow-lg">
                 <h1 className="text-2xl font-bold mb-4">{interviewData.company.name}</h1>
+
+                <div className="text-lg mb-2">
+                    <strong>Company Description:</strong> {interviewData.company.description}
+                </div>
+
+                <div className="text-lg mb-2">
+                    <strong>Company Tel:</strong> {interviewData.company.tel}
+                </div>
 
                 <div className="text-lg mb-2">
                     <strong>Interview Date:</strong> {formatDate(interviewData.interviewDate)}
@@ -33,14 +42,11 @@ export default async function InterviewDetailPage({ params }: { params: { wid: s
                 <div className="text-lg mb-2">
                     <strong>Created At:</strong> {formatDate(interviewData.createdAt)}
                 </div>
-
-                <div className="text-lg mb-2">
-                    <strong>Username:</strong> {interviewData.user.name}
-                </div>
-
-                <div className="text-lg mb-2">
-                    <strong>Company Description:</strong> {interviewData.company.description}
-                </div>
+  
+            </div>
+            <div className="flex flex-row justify-around mt-4 max-w-4xl mx-auto">
+                <UpdateComponent interviewId={interviewId} token={token}/>
+                <DeleteComponent interviewId={interviewId} token={token}/>
             </div>
         </main>
     );
