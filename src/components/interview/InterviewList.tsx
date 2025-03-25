e142a5052c0cb09457dd1da9d008876d961644ae
Purinut Jamma <poorinut.frame2547@gmail.com>
2025-03-25 19:43:12 +0700

    Update InterviewList style

--- src/components/interview/InterviewList.tsx
+++ src/components/interview/InterviewList.tsx
@@ -17,14 +17,14 @@ export default function InterviewList( {interviewJson, token}:{interviewJson:Int
             {
                 interviewJson.data.map((interveiwItem:InterviewItem)=>(
                     <Link href={`/mybooking/${interveiwItem._id}`} key={interveiwItem._id}>
-                        <div className="bg-secondary rounded-md px-5 mx-5 py-2 my-2 hover:bg-purple-300 transition" key={interveiwItem._id}>
+                        <div className="bg-secondary rounded-md px-5 mx-10 pt-2 pb-4 my-4 hover:bg-purple-300 transition" key={interveiwItem._id}>
                             <table className="table-auto border-separate border-spacing-2 text-gray-700"><tbody>
-                                <tr><td className="col-span-2 text-2xl">{interveiwItem.company.name}</td></tr>
+                                <tr><td className="col-span-2 text-2xl font-bold">{interveiwItem.company.name}</td></tr>
                                 {
-                                (interviewJson.userRole === "admin") ? <tr><td>Username:</td><td>{interveiwItem.user.name}</td></tr> : null   
+                                (interviewJson.userRole === "admin") ? <tr><td className="font-medium">Username:</td><td>{interveiwItem.user.name}</td></tr> : null   
                                 }
-                                <tr><td>Interview Date:</td><td>{formatDate(interveiwItem.interviewDate)}</td></tr>
-                                <tr><td>Created:</td><td>{formatDate(interveiwItem.createdAt)}</td></tr>
+                                <tr><td className="font-medium">Interview Date:</td><td>{formatDate(interveiwItem.interviewDate)}</td></tr>
+                                <tr><td className="font-medium">Created:</td><td>{formatDate(interveiwItem.createdAt)}</td></tr>
                             </tbody></table>
                             <div className="flex flex-row gap-2">
                                 <UpdateComponent interviewId={interveiwItem._id} token={token}/>
